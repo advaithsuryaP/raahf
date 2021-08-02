6d394bd72fa7bea4077417f85d0c0d763e8b2ccc
advaithsuryaP <padvaithsurya@gmail.com>
2021-08-02 17:06:30 +0530

    Changed learning header component location

--- raahfui/src/app/learning/learning.module.ts
+++ raahfui/src/app/learning/learning.module.ts
@@ -6,13 +6,13 @@ import { MaterialModule } from '../material/material.module';
 import { LearningRoutingModule } from './learning-routing.module';
 
 import { LearningComponent } from './learning/learning.component';
-import { LearningHeaderComponent } from './learning-header/learning-header.component';
 import { LearningHomeComponent } from './learning/learning-home/learning-home.component';
 import { BookEditComponent } from './learning/learning-library/book-edit/book-edit.component';
 import { LearningLibraryComponent } from './learning/learning-library/learning-library.component';
 import { LearningGeographyComponent } from './learning/learning-geography/learning-geography.component';
 import { LearningHistoryComponent } from './learning/learning-history/learning-history.component';
 import { ChapterEditComponent } from './learning/learning-library/book-edit/chapter-edit/chapter-edit.component';
+import { LearningHeaderComponent } from './learning/learning-header/learning-header.component';
 
 
 @NgModule({
